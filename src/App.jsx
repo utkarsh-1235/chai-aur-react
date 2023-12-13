499fe620e13d420c3c97123ca9f4ac73d122285a
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-12-13 15:20:24 +0530

    add and remove value using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0)
 
+  const addvalue = ()=>{
+    if(count<20){
+      count = count+1
+      setCount(count)
+    }
+    else{
+      setCount(0)
+    }
+    
+  }
+
+  const deletevalue = ()=>{
+    if(count>0){
+      count = count-1
+      setCount(count)
+    }
+    else{
+      setCount(0)
+    }
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <h1> Chai Code Aur React</h1>
+          <h2> value is: {count}</h2>
+          <button onClick={addvalue}>Add Value </button>
+          <button onClick={deletevalue}>Remove Value</button>
     </>
   )
 }
